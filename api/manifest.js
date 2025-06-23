50fbc643c00d8c4740b021bccbe69afeaa467cfa
Sz19-pixel <trytry11101@gmail.com>
2025-06-23 04:20:24 +0300

    Create manifest.js

---
+++ api/manifest.js
@@ -0,0 +1,35 @@
+// Manifest endpoint
+export default function handler(req, res) {
+    const manifest = {
+        id: 'myflixer.stremio.addon',
+        version: '1.0.0',
+        name: 'MyFlixer',
+        description: 'Watch movies and series from MyFlixer',
+        logo: 'https://myflixerz.to/images/group_1/theme_7/logo.png?v=0.1',
+        resources: ['catalog', 'meta', 'stream'],
+        types: ['movie', 'series'],
+        catalogs: [
+            {
+                type: 'movie',
+                id: 'myflixer-movies',
+                name: 'MyFlixer Movies',
+                extra: [
+                    { name: 'skip', isRequired: false },
+                    { name: 'search', isRequired: false }
+                ]
+            },
+            {
+                type: 'series',
+                id: 'myflixer-series',
+                name: 'MyFlixer TV Series',
+                extra: [
+                    { name: 'skip', isRequired: false },
+                    { name: 'search', isRequired: false }
+                ]
+            }
+        ],
+        idPrefixes: ['myflixer:']
+    };
+
+    res.status(200).json(manifest);
+}
